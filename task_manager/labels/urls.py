0b6b8bb0dd346cb21afb149999e89cc3e463c7e7
Alexander Pavlov <dev@pavlovteam.ru>
2024-09-19 21:57:31 +0300

    format

--- task_manager/labels/urls.py
+++ task_manager/labels/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
-from task_manager.labels.views import LabelsList, CreateLabel, UpdateLabel,\
-    DeleteLabel
+from task_manager.labels.views import LabelsList, CreateLabel, UpdateLabel, DeleteLabel
 
 app_name = 'labels'
 urlpatterns = [
