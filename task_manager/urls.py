5cd8351d25dfac10bda4a434b610f3f846344197
Alexander Pavlov <dev@pavlovteam.ru>
2024-10-13 17:13:51 +0300

    refactor: fix linter

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -13,9 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include, re_path
-from django.views.generic import TemplateView
 
 from task_manager import settings
 from task_manager.users.views import LoginUser, LogoutUser, IndexView
@@ -23,14 +23,21 @@ from task_manager.users.views import LoginUser, LogoutUser, IndexView
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('users/', include('task_manager.users.urls'), name='users_list'),
-    path('statuses/', include('task_manager.statuses.urls'),
-         name='statuses_list'),
+    path(
+        'statuses/', include('task_manager.statuses.urls'), name='statuses_list'
+    ),
     path('labels/', include('task_manager.labels.urls'), name='labels_list'),
-    re_path(r'tasks/',
-            include('task_manager.tasks.urls', namespace='task'),
-            name='tasks_list'),
+    re_path(
+        r'tasks/',
+        include('task_manager.tasks.urls', namespace='task'),
+        name='tasks_list',
+    ),
     path('login/', LoginUser.as_view(), name='login'),
-    path('logout/', LogoutUser.as_view(),
-         {'next_page': settings.LOGOUT_REDIRECT_URL}, name='logout'),
+    path(
+        'logout/',
+        LogoutUser.as_view(),
+        {'next_page': settings.LOGOUT_REDIRECT_URL},
+        name='logout',
+    ),
     path('admin/', admin.site.urls),
 ]
