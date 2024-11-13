c303c406f1c01a52b680a2fdfb11091485d2606a
Alexander Pavlov <dev@pavlovteam.ru>
2024-11-13 21:21:19 +0300

    fix: change migrations

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -3,10 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    theme_mode = CharField(
-        max_length=10,
-        default='dark',
-    )
 
     def __str__(self):
         return self.get_full_name()
