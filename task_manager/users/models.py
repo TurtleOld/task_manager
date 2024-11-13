b4896cdb9b541b4fb4c697c28e88fdb8588c5d4d
Alexander Pavlov <dev@pavlovteam.ru>
2024-11-13 21:18:01 +0300

    fix: change migrations

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -1,17 +1,11 @@
 from django.db.models import CharField
 from django.contrib.auth.models import AbstractUser
 
-THEME_CHOICES = [
-    ('dark', 'dark'),
-    ('light', 'light'),
-]
-
 
 class User(AbstractUser):
     theme_mode = CharField(
         max_length=10,
         default='dark',
-        choices=THEME_CHOICES,
     )
 
     def __str__(self):
