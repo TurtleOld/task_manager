998127ae51e02ee491400e2bd9ee129819bc46ab
Alexander Pavlov <dev@pavlovteam.ru>
2024-11-25 15:32:44 +0300

    refactor: change variable name

--- task_manager/context_processors.py
+++ task_manager/context_processors.py
@@ -6,8 +6,8 @@ from django.http import HttpRequest
 
 def theme_mode(request: HttpRequest) -> dict[str, Any]:
     if isinstance(request.user, AnonymousUser):
-        theme_mode = 'light'
+        mode = 'light'
     else:
-        theme_mode = request.user.theme_mode
+        mode = request.user.theme_mode
 
-    return {'theme_mode': theme_mode}
+    return {'theme_mode': mode}
