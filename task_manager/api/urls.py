ed3993ef645b90212b7ac607b21a6a9c2529e094
Alexander Pavlov <dev@pavlovteam.ru>
2025-09-12 09:21:55 +0300

    🐞 fix: error 404 apis

--- task_manager/api/urls.py
+++ task_manager/api/urls.py
@@ -9,7 +9,5 @@ urlpatterns = [
     path('auth/login/', LoginView.as_view(), name='login'),
     path('auth/logout/', LogoutView.as_view(), name='logout'),
     path('auth/profile/', UserProfileView.as_view(), name='profile'),
-
-    # Включаем URL-ы из других приложений
     path('users/', include('task_manager.users.urls')),
 ]
