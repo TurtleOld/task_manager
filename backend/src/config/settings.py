7742b546bb1d2368ff3f7210512d34340dec4e2d
Alexander Pavlov <dev@pavlovteam.ru>
2026-04-07 12:34:59 +0300

    build(release): advance app and service versions to 1.0.7

--- backend/src/config/settings.py
+++ backend/src/config/settings.py
@@ -193,6 +193,6 @@ REST_FRAMEWORK = {
 SPECTACULAR_SETTINGS = {
     "TITLE": "Task Manager API",
     "DESCRIPTION": "Kanban API for Task Manager",
-    "VERSION": "1.0.6",
+    "VERSION": "1.0.7",
     "SERVE_INCLUDE_SCHEMA": False,
 }
