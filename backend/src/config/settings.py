cc8f575247b3e413a938d13b565c36c478ba1f38
Alexander Pavlov <dev@pavlovteam.ru>
2026-05-08 21:11:12 +0300

    chore: bump version to 1.0.15

--- backend/src/config/settings.py
+++ backend/src/config/settings.py
@@ -193,6 +193,6 @@ REST_FRAMEWORK = {
 SPECTACULAR_SETTINGS = {
     "TITLE": "Task Manager API",
     "DESCRIPTION": "Kanban API for Task Manager",
-    "VERSION": "1.0.13",
+    "VERSION": "1.0.15",
     "SERVE_INCLUDE_SCHEMA": False,
 }
