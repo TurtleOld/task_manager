1d08f8bfe0c1b451c56b39134980a04d397d2cb6
Alexander Pavlov <dev@pavlovteam.ru>
2026-09-26 12:57:03 +0300

    refactor: упростить регистронезависимый поиск упомянутых пользователей
    Один запрос через annotate(Lower(...)) вместо reduce/or_ по Q-объектам.

--- backend/src/kanban/views/cards.py
+++ backend/src/kanban/views/cards.py
@@ -1,14 +1,13 @@
 from __future__ import annotations
 
 import re
-from functools import reduce
-from operator import or_
 from typing import Any
 
 from django.contrib.auth import get_user_model
 from django.core.files.storage import default_storage
 from django.db import transaction
-from django.db.models import F, Q
+from django.db.models import F
+from django.db.models.functions import Lower
 from django.utils import timezone
 from django.utils.text import get_valid_filename
 from rest_framework import permissions, status, viewsets
@@ -429,11 +428,13 @@ class CardViewSet(viewsets.ModelViewSet[Card]):
         additive.
         """
 
-        usernames = set(re.findall(r"@([\w.@+-]+)", comment.text))
+        usernames = {
+            item.lower() for item in re.findall(r"@([\w.@+-]+)", comment.text)
+        }
         mentioned_users = (
-            User.objects.filter(
-                reduce(or_, (Q(username__iexact=name) for name in usernames))
-            ).exclude(id=comment.author_id)
+            User.objects.annotate(username_lower=Lower("username"))
+            .filter(username_lower__in=usernames)
+            .exclude(id=comment.author_id)
             if usernames
             else User.objects.none()
         )
